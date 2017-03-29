612eeb9a3b7beb210633c52d2c9fd06c72afcf05
renhongyi <renhongyi@interrcs.com>
2017-03-29 17:02:39 +0800

    add mongo

--- spiders/dmoz_spider.py
+++ spiders/dmoz_spider.py
@@ -44,48 +44,53 @@ class DmozSpider(scrapy.Spider):
         print filename
         with open('verify', 'wb') as f:
             f.write(response.body)
+            from pymongo import MongoClient
+            client = MongoClient('localhost', 27017)
+            database = client.get_database("local")
+            collection = database.get_collection("aaaa")
+            collection.insert_one({"body": response.body.decode('gbk').encode('utf8')})
 
-    # def login(self, response):
-    #     current_time = str(int(time.time() * 10))
-    #     verify = "http://www.wdsz.net/ck.php?nowtime=" + current_time
-    #     return [Request(verify, callback=self.parse)]
-    #
-    #     # data = {
-    #     #     "form_email": "XXXX",
-    #     #     "form_password": "XXXX",
-    #     #     "captcha-solution": captcha_value,
-    #     # }
-    #     # return [FormRequest.from_response(response,
-    #     #                                   meta={"cookiejar": response.meta["cookiejar"]},
-    #     #                                   headers=self.UserAgent,
-    #     #                                   formdata=data,
-    #     #                                   callback=self.crawlerdata,
-    #     #                                   )]
-    #
-    # def parse(self, response):
-    #     filename = response.url.split("/")[-1]
-    #     print filename
-    #     with open('verify', 'wb') as f:
-    #         f.write(response.body)
-    #         f.close()
-    #         print self.image_to_string('1.png')
-    #         # image_to_string('verify')
-    #
-    # # def image2string(self, img):
-    # #     file1 = open('verify', 'r+b')
-    # #     from PIL import Image
-    # #     im = Image.open(file1)
-    # #     text = image_to_string(im)
-    # #     print "Using image_to_string(): "
-    # #     print text
-    #
-    # def image_to_string(self, img, cleanup=True, plus=''):
-    #     # subprocess.check_output('tesseract ' + img + ' ' + plus, shell=True)
-    #     # text = ''
-    #     # with open(img + 'txt', 'r') as f:
-    #     #     text = f.read().strip()
-    #     # if cleanup:
-    #     #     os.remove(img + 'txt')
-    #     # return text
-    #     res = subprocess.check_output('tesseract ' + img + ' stdout').decode()
-    #     return (re.subn('\W', '', res.strip()) if res else ('', ''))[0].lower()
+            # def login(self, response):
+            #     current_time = str(int(time.time() * 10))
+            #     verify = "http://www.wdsz.net/ck.php?nowtime=" + current_time
+            #     return [Request(verify, callback=self.parse)]
+            #
+            #     # data = {
+            #     #     "form_email": "XXXX",
+            #     #     "form_password": "XXXX",
+            #     #     "captcha-solution": captcha_value,
+            #     # }
+            #     # return [FormRequest.from_response(response,
+            #     #                                   meta={"cookiejar": response.meta["cookiejar"]},
+            #     #                                   headers=self.UserAgent,
+            #     #                                   formdata=data,
+            #     #                                   callback=self.crawlerdata,
+            #     #                                   )]
+            #
+            # def parse(self, response):
+            #     filename = response.url.split("/")[-1]
+            #     print filename
+            #     with open('verify', 'wb') as f:
+            #         f.write(response.body)
+            #         f.close()
+            #         print self.image_to_string('1.png')
+            #         # image_to_string('verify')
+            #
+            # # def image2string(self, img):
+            # #     file1 = open('verify', 'r+b')
+            # #     from PIL import Image
+            # #     im = Image.open(file1)
+            # #     text = image_to_string(im)
+            # #     print "Using image_to_string(): "
+            # #     print text
+            #
+            # def image_to_string(self, img, cleanup=True, plus=''):
+            #     # subprocess.check_output('tesseract ' + img + ' ' + plus, shell=True)
+            #     # text = ''
+            #     # with open(img + 'txt', 'r') as f:
+            #     #     text = f.read().strip()
+            #     # if cleanup:
+            #     #     os.remove(img + 'txt')
+            #     # return text
+            #     res = subprocess.check_output('tesseract ' + img + ' stdout').decode()
+            #     return (re.subn('\W', '', res.strip()) if res else ('', ''))[0].lower()
